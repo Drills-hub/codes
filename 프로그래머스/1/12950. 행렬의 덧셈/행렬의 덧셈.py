b873a494dab460c4d3e1515235bf57b3436ee186
Drills-hub <sdi6566@gmail.com>
2024-07-23 14:19:15 +0900

    [level 1] Title: 행렬의 덧셈, Time: 25.49 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,8 +1,8 @@
 def solution(arr1, arr2):
     answer = []
     for a, b in zip(arr1, arr2):
-        Mat_plus = []
+        mat_plus = []
         for c, d in zip(a, b):
-            Mat_plus.append(c + d)
-        answer.append(Mat_plus)
+            mat_plus.append(c + d)
+        answer.append(mat_plus)
     return answer
