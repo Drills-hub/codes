41f5b3cbee73741112c04b2a456a6108a3b9dfd1
Drills-hub <sdi6566@gmail.com>
2024-07-25 16:57:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,13 @@
+
+def solution(s):
+    answer = ''
+    s_list = s.split(' ')
+    for i in s_list:
+        for j in range(len(i)):
+            if j&1==0:
+                answer += i[j].upper()
+            else:
+                answer += i[j].lower()
+        answer += ' '
+
+    return answer[:-1]
