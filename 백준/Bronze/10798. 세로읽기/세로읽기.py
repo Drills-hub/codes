83c9689b87a55a92b8344e3515ee732be53eb64b
Drills-hub <sdi6566@gmail.com>
2024-07-26 14:59:14 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,19 @@
+# 5행 n열 행렬 만들기
+def make_matrix():
+    matrix = []
+    for _ in range(5):
+        row = input()
+        matrix.append(row)
+    return matrix
+
+# 행려을 세로로 읽게 만들기
+def read_column(matrix):
+    for j in range(15):
+        for i in range(5):
+            if j < len(matrix[i]):
+                print(matrix[i][j], end='')
+    print()
+
+
+matrix_a = make_matrix()
+read_column_a = read_column(matrix_a)
