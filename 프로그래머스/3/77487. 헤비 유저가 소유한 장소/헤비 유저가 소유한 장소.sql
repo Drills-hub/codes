5fd0d0a473ebce5da8b01824657f7ff7ea969123
Drills-hub <sdi6566@gmail.com>
2024-08-02 20:02:41 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,8 @@
+SELECT P.ID, P.NAME, PS.HOST_ID
+FROM PLACES P JOIN(
+                    SELECT HOST_ID
+                    FROM PLACES
+                    GROUP BY 1
+                    HAVING COUNT(HOST_ID) >= 2
+                    ) PS ON P.HOST_ID = PS.HOST_ID
+ORDER BY 1
