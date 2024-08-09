fae60269592dc61c84c2ce77f059ab981a6bfbe9
Drills-hub <sdi6566@gmail.com>
2024-08-09 10:49:57 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.84 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,26 @@
+
+"""
+코드 설계
+1. 문자열을 하나씩 나눠 인덱스와 알파벳을 따로 딕셔너리로 저장
+2. 문자열을 글자 하나 씩 비교해서 
+   앞에 같은 글자가 있었는지 확인
+3. 앞에 없으면 -1, 있으면 인덱스를 비교해서 숫자 변환
+4. 결과값 출력
+
+"""
+def solution(s):
+    answer = []
+    idx = {}
+
+    for i, j in enumerate(s):
+        if j in idx:
+            dif = i - idx[j] + 1
+            answer.append(dif)
+            idx[j] = i + 1
+        else:
+            answer.append(-1)
+            idx[j] = i + 1
+
+
+
+    return answer
