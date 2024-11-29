6ccf2cd69701debd76972c1dd58e1e75b512de4a
Drills-hub <sdi6566@gmail.com>
2024-11-29 12:41:13 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,14 +1,12 @@
 def solution(N):
 
-    answer = []
     list_array = input().split()
     v = input()
 
-    for i in range(len(list_array)):
-        if list_array[i] == v:
-            answer.append(list_array[i])
-    return print(len(answer))
+    count = list_array.count(v)
+
+    return count
 
 
 N = int(input())
-solution(N)
+print(solution(N))
