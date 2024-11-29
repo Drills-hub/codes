78dc07098dfbc3151ad9b5b84869e6a3bbd58700
Drills-hub <sdi6566@gmail.com>
2024-11-29 13:15:20 +0900

    [Bronze III] Title: 최소, 최대, Time: 336 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,7 +1,14 @@
 def solution(N):
     list_array = list(map(int, (input().split())))
-    answer = [str(min(list_array)), str(max(list_array))]
-    return " ".join(answer)
+    min_num, max_num = float("inf"), float("-inf")
+
+    for num in list_array:
+        if num < min_num:
+            min_num = num
+        if num > max_num:
+            max_num = num
+
+    return f"{min_num} {max_num}"
 
 
 N = input()
