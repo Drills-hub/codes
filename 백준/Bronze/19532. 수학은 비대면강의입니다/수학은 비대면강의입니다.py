99871e19674967dde964a00bc7b7d9d662955011
Drills-hub <sdi6566@gmail.com>
2025-04-18 12:48:10 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,6 +1,2 @@
-a,b,c,d,e,f = map(int, input().split())
-
-y = (a*f-c*d)//(a*e-d*b)
-x = (c*e-b*f)//(a*e-d*b)
-
-print(x,y)
+a, b, c, d, e, f = map(int, input().split())
+print((c*e-b*f)//(a*e-b*d), (a*f-c*d)//(a*e-b*d))
