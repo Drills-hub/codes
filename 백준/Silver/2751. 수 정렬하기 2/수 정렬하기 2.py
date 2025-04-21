c4566d7fe36a079dc3389e52d91027e7e5548324
Drills-hub <sdi6566@gmail.com>
2025-04-21 14:05:54 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1112 ms, Memory: 118856 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -2,12 +2,14 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-count = [0] * 2000001  # -1,000,000 ~ 1,000,000
+count = [0] * 2000001  
 
 for _ in range(N):
     n = int(input())
-    count[n + 1000000] += 1  # 인덱스 보정
-
+    count[n + 1000000] += 1  
+result = []
 for i in range(2000001):
-    for _ in range(count[i]):
-        print(i - 1000000)
+    if count[i] > 0:
+        result.extend([str(i - 1000000)] * count[i])
+
+sys.stdout.write('\n'.join(result) + '\n')
