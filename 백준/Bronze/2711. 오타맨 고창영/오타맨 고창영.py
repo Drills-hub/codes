49b3c5b7b9e0ea4c90ec1bb2df9d95b29760cff9
Drills-hub <sdi6566@gmail.com>
2025-05-01 15:26:01 +0900

    [Bronze II] Title: 오타맨 고창영, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2711. 오타맨 고창영/오타맨 고창영.py
+++ 백준/Bronze/2711. 오타맨 고창영/오타맨 고창영.py
@@ -1,7 +1,7 @@
-N=int(input())
+def typo(N):
+    for _ in range(N):
+        a,b=input().split()
+        a=int(a)
+        print(b[:a-1] + b[a:])
 
-for _ in range(N):
-    a,b=input().split()
-    a=int(a)
-    result = b[:a-1] + b[a:]
-    print(result)
+typo(int(input()))
