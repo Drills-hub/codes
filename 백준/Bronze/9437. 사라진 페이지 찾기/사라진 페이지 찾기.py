50c4a525cfe881d0574048ac15c3fd3ccae049d6
Drills-hub <sdi6566@gmail.com>
2025-05-10 17:07:59 +0900

    [Bronze II] Title: 사라진 페이지 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9437. 사라진 페이지 찾기/사라진 페이지 찾기.py
+++ 백준/Bronze/9437. 사라진 페이지 찾기/사라진 페이지 찾기.py
@@ -1,28 +1,23 @@
 while True:
-    Num = input().split()
+    num = input().split()
     answer = []
 
-    if Num == ["0"]:
+    if num == ["0"]:
         break
 
-    N = int(Num[0])
-    P = int(Num[1])
+    N = int(num[0])
+    P = int(num[1])
 
     if P%2 == 0:
         i=P-1
-        answer.append(i)
         j=(N+1)-P
-        answer.append(j)
         k=(N+1)-i
-        answer.append(k)
     else:
         i=P+1
-        answer.append(i)
         j=(N+1)-P
-        answer.append(j)
         k=(N+1)-i
-        answer.append(k)
 
+    answer.extend([i, j, k])
     answer.sort()
     answer = list(map(str, answer))
     print(" ".join(answer))
