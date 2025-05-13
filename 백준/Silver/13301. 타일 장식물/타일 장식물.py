435d16c3b30db51443e4e5fce9bedac48af1cd94
Drills-hub <sdi6566@gmail.com>
2025-05-13 17:02:20 +0900

    [Silver V] Title: 타일 장식물, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * (n + 1)
+
+dp[0] = 0
+if n >= 1:
+    dp[1] = 1
+
+for i in range(2, n + 1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+answer=(dp[n]+dp[n-1])*2+(dp[n]*2)
+print(answer)
