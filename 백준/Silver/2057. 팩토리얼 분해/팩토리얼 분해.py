b258db16782e7c80b3f4aa3cbf0ba98d85007a6b
Drills-hub <sdi6566@gmail.com>
2025-05-27 13:00:14 +0900

    [Silver V] Title: 팩토리얼 분해, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.py
+++ 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.py
@@ -2,23 +2,22 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-factor = [1]
-for i in range(1, 21):
-    next_fact = factor[-1] * i
-    if next_fact > n:
-        break
-    factor.append(next_fact)
 
-L = len(factor)
-for mask in range(1, 1 << L):
-    total = 0
-    for i in range(L):
-        if mask & (1 << i):
-            total += factor[i]
-            if total > n:
-                break
-    if total == n:
-        print("YES")
-        break
-else:
+factor = [1, 1]  
+for i in range(2, 21):  
+    factor.append(factor[-1] * i)
+
+factor = sorted(factor,reverse=True)
+
+
+if n == 0:
     print("NO")
+else:
+    for num in factor:
+        if n - num >= 0:
+            n -= num
+            if n == 0:
+                print("YES")
+                break
+    else:
+        print("NO")
