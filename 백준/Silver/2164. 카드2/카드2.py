d0bc7e00fdc6121d7ddd300bbfe9f6235f89f108
Drills-hub <sdi6566@gmail.com>
2025-07-07 18:56:37 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 51848 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,10 @@
+from collections import deque
+n = int(input())
+
+card_queue = deque(range(1, n+1))
+
+while len(card_queue) != 1:
+    card_queue.popleft()
+    card_queue.append(card_queue.popleft())
+
+print(card_queue[0])
