4885db0e94ff991b681ca656c7983d05ed45229f
Drills-hub <sdi6566@gmail.com>
2025-08-11 12:58:45 +0900

    [Silver V] Title: 문서 검색, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -1,14 +1,5 @@
 A=input()
 B=input()
 
-i = 0
-count = 0
-while i <= len(A) - len(B):
-
-    if A[i:i+len(B)] == B:
-        count += 1
-        i += len(B)
-    else:
-        i += 1
-
-print(count)
+answer = A.count(B)
+print(answer)
