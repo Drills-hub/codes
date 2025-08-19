1e60bf6f593a24a448b565221f01d65cbecd0c01
Drills-hub <sdi6566@gmail.com>
2025-08-19 16:06:26 +0900

    [Bronze III] Title: K 2K 게임, Time: 56 ms, Memory: 39944 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
+++ 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
@@ -1,14 +1,13 @@
 import sys
 
 input = sys.stdin.readline
-N, K = map(int, (input().split()))
+N, K = map(int, input().split())
 
-K = K % 10
+exclude_digits = {K % 10, (2 * K) % 10}
 answer = []
-
 for i in range(1, N + 1):
-    if i % 10 != K and i % 10 != K * 2:
+    if i % 10 not in exclude_digits:
         answer.append(i)
-
+        
 print(len(answer))
 print(" ".join(map(str, answer)))
