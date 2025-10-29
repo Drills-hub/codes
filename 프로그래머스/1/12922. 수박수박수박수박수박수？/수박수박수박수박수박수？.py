bede0b22f17da55875a6f2e71a11b679c3f93784
Drills-hub <sdi6566@gmail.com>
2025-10-29 23:16:49 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.98 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,6 +1,9 @@
 def solution(n):
-    answer = '수박'
-    if n % 2 ==0:
-        return answer * (n//2)
-    else:
-        return answer * (n//2) + answer[:1]
+    answer = []
+    for i in range(1,n+1):
+        if i % 2==0:
+            answer.append('박')
+        else:
+            answer.append('수')
+
+    return ''.join(answer)
